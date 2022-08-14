7193c26eb2ae7def9aca2dff5ec1a5cd913cbe0d
rickyraz <rickyraihan83@gmail.com>
2022-08-15 06:48:13 +0700

    update-code

--- src/restaurant/mod.rs
+++ src/restaurant/mod.rs
@@ -1,34 +1,37 @@
-mod pizza_order{
+mod pizza_order {
     pub struct Pizza {
-        pub dough : String,
-        pub cheese : String,
-         pub topping : String,
+        pub dough: String,
+        pub cheese: String,
+        pub topping: String,
     }
     impl Pizza {
-        pub fn lunch(topping: &str) -> Pizza{
-            Pizza  {
+        pub fn lunch(topping: &str) -> Pizza {
+            Pizza {
                 dough: String::from("Regular dough"),
                 cheese: String::from("mozarella"),
-                topping:String::from(topping),
+                topping: String::from(topping),
             }
         }
     }
 
     pub mod help_cutomer {
-        fn seat_at_a_table(){
+        fn seat_at_a_table() {
             println!("Customer seated at a table");
         }
-        pub fn take_order(){
+        pub fn take_order() {
             seat_at_a_table();
-            let cust_pizza : super::Pizza = super::Pizza::lunch("veggies");
+            let cust_pizza: super::Pizza = super::Pizza::lunch("veggies");
             serve_customer(cust_pizza)
         }
-        fn serve_customer(cutz_pizza: super::Pizza){
-            println!("The customer is served a regular pizza with {}", cutz_pizza.topping);
+        fn serve_customer(cutz_pizza: super::Pizza) {
+            println!(
+                "The customer is served a regular pizza with {}",
+                cutz_pizza.topping
+            );
         }
     }
 }
 
-pub fn order_food(){
+pub fn order_food() {
     crate::restaurant::pizza_order::help_cutomer::take_order();
 }
